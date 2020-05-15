84210947e2381bbc07ad642bed94fcc5c2891913
AfterLogic Support <support@afterlogic.com>
2020-05-15 15:50:45 +0300

    current password verification added

--- Module.php
+++ Module.php
@@ -12,7 +12,7 @@ namespace Aurora\Modules\MailChangePasswordIspconfigPlugin;
  * 
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
- * @copyright Copyright (c) 2019, Afterlogic Corp.
+ * @copyright Copyright (c) 2020, Afterlogic Corp.
  *
  * @package Modules
  */
@@ -90,6 +90,30 @@ class Module extends \Aurora\System\Module\AbstractModule
 		return $bFound;
 	}
 
+	protected function crypt_password($cleartext_password) {
+		if(defined('CRYPT_SHA512') && CRYPT_SHA512 == 1) {
+			$salt = '$6$rounds=5000$';
+			$salt_length = 16;
+		} elseif(defined('CRYPT_SHA256') && CRYPT_SHA256 == 1) {
+			$salt = '$5$rounds=5000$';
+			$salt_length = 16;
+		} else {
+			$salt = '$1$';
+			$salt_length = 12;
+		}
+
+		if(function_exists('openssl_random_pseudo_bytes')) {
+			$salt .= substr(bin2hex(openssl_random_pseudo_bytes($salt_length)), 0, $salt_length);
+		} else {
+			$base64_alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789./';
+			for($n = 0; $n < $salt_length; $n++) {
+				$salt .= $base64_alphabet[mt_rand(0, 63)];
+			}
+		}
+		$salt .= "$";
+		return crypt($cleartext_password, $salt);
+	}
+	
 	/**
 	 * Tries to change password for account.
 	 * @param \Aurora\Modules\Mail\Classes\Account $oAccount
@@ -100,7 +124,8 @@ class Module extends \Aurora\System\Module\AbstractModule
 	protected function changePassword($oAccount, $sPassword)
 	{
 	    $bResult = false;
-	    if (0 < strlen($oAccount->getPassword()) && $oAccount->getPassword() !== $sPassword )
+		$sPassCurr = $oAccount->getPassword();
+	    if (0 < strlen($sPassCurr) && $sPassCurr !== $sPassword )
 	    {
 			$config_dbuser = $this->getConfig('DbUser','');
 			$config_dbpass = $this->getConfig('DbPass','');
@@ -110,12 +135,21 @@ class Module extends \Aurora\System\Module\AbstractModule
 			$mysqlcon = mysqli_connect($config_dbhost, $config_dbuser, $config_dbpass, $config_dbname);
 			if ($mysqlcon)
 			{
-				$sPasshash = exec("doveadm pw -s 'ssha512' -p '" . $sPassword . "'");
-				$sql = "UPDATE mailbox SET password='" . $sPasshash . "' WHERE username='" . $oAccount->IncomingLogin . "'";
-				$bResult = mysqli_query($mysqlcon,$sql);
-				if (!$bResult)
-				{
-					throw new \Aurora\System\Exceptions\ApiException(\Aurora\System\Exceptions\Errs::UserManager_AccountNewPasswordUpdateError);
+				$sql = "SELECT * FROM mail_user WHERE login='" . $oAccount->IncomingLogin . "'";
+				$result = mysqli_query($mysqlcon,$sql);
+				$aUser = mysqli_fetch_array($result);
+				$sPassStored = stripslashes($aUser['password']);
+			    $sSalt = substr($sPassStored,0,1+strrpos($sPassStored,'$'));
+				if(crypt(stripslashes($sPassCurr),$sSalt) == $sPassStored) { 
+					throw new \Aurora\System\Exceptions\ApiException(\Aurora\System\Exceptions\Errs::UserManager_AccountOldPasswordNotCorrect);
+				} else {
+					$sPasshash = crypt_password($sPassword);
+					$sql = "UPDATE mail_user SET password='" . $sPasshash . "' WHERE email='" . $oAccount->IncomingLogin . "'";
+					$bResult = mysqli_query($mysqlcon,$sql);
+					if (!$bResult)
+					{
+						throw new \Aurora\System\Exceptions\ApiException(\Aurora\System\Exceptions\Errs::UserManager_AccountNewPasswordUpdateError);
+					}
 				}
 				mysqli_close($mysqlcon);
 			}
