935b16b433c822c4ea8af2bb6e4870c2584696af
AfterLogic Support <support@afterlogic.com>
2020-05-15 16:12:14 +0300

    current password verification adjusted

--- Module.php
+++ Module.php
@@ -140,7 +140,7 @@ class Module extends \Aurora\System\Module\AbstractModule
 				$aUser = mysqli_fetch_array($result);
 				$sPassStored = stripslashes($aUser['password']);
 			    $sSalt = substr($sPassStored,0,1+strrpos($sPassStored,'$'));
-				if(crypt(stripslashes($sPassCurr),$sSalt) == $sPassStored) { 
+				if(crypt(stripslashes($sPassCurr),$sSalt) !== $sPassStored) { 
 					throw new \Aurora\System\Exceptions\ApiException(\Aurora\System\Exceptions\Errs::UserManager_AccountOldPasswordNotCorrect);
 				} else {
 					$sPasshash = crypt_password($sPassword);
